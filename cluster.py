f3d9840731e81c0b89b5a642d7d6c55e22929c92
Ananya Agarwal <aagarwal@dons.usfca.edu>
2024-02-21 10:30:53 -0800

    Questions 1,2, 3

--- cluster.py
+++ cluster.py
@@ -1,7 +1,25 @@
+import numpy as np
 class cluster:
 
-    def __init__(self):
-        pass
+    def __init__(self, k=5, max_iterations=100):
+        self.k = k
+        self.max_iterations = max_iterations
 
     def fit(self, X):
-        pass
+        X = np.array(X)
+        n, d = X.shape
+
+        # Choosing centroids
+        centroids = X[np.random.choice(n, self.k, replace=False)]
+
+        for _ in range(self.max_iterations):
+            clusters = np.argmin(np.linalg.norm(X[:, np.newaxis] - centroids, axis=2), axis=1)
+
+            new_centroids = [np.mean(X[np.array(clusters) == i], axis=0) for i in range(self.k)]
+
+            if np.all(np.equal(centroids, new_centroids)):
+                break
+
+            centroids = new_centroids
+
+        return clusters, centroids
